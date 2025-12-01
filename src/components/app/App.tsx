28173e71801e3d639119cac2a8897e32bbd5447d
Olena Petrova <petrovahel@icloud.com>
2025-12-01 03:16:57 +0200

    fix import

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from '../cafeInfo/CafeInfo';
+import CafeInfo from '../CafeInfo/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
-import VoteOptions from '../voteOptions/VoteOptions';
-import VoteStats from '../voteStats/VoteStats';
-import Notification from '../notification/Notification';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 export default function App() {
 
