4e0018b38ac2ff62a5cf81059834b58f22337902
Olena Petrova <petrovahel@icloud.com>
2025-12-01 03:13:00 +0200

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "../src/components/App/App";
+import App from "./components/App/App";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
 
