e63bbf0d2f6075a7ae1a4da31bb34c63e449f74f
Olena Petrova <petrovahel@icloud.com>
2025-12-01 04:03:04 +0200

    fix name of folders

--- src/components/App_temp/App.tsx
+++ src/components/App_temp/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import css from './App.module.css';
-import CafeInfo from '../CafeInfo/CafeInfo';
+import CafeInfo from '../CafeInfodfeveve/CafeInfo';
 import type { Votes, VoteType } from '../../types/votes';
-import VoteOptions from '../VoteOptions/VoteOptions';
-import VoteStats from '../VoteStats/VoteStats';
-import Notification from '../Notification/Notification';
+import VoteOptions from '../VoteOptionsrvfwrv/VoteOptions';
+import VoteStats from '../VoteStatsergwegw/VoteStats';
+import Notification from '../Notificationcfwevf/Notification';
 
 export default function App() {
 
